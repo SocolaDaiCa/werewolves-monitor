9eac5d53857eb3bfe0a95e2922a63c6cf55f6374
tiennt <tokentien@gmail.com>
2025-11-16 17:08:39 +0700

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,54 +10,56 @@
       </div>
 
       <!-- Main Menu Cards -->
-      <div class="flex flex-col gap-4">
-        <!-- Start Game Button -->
-        <ion-button 
-          expand="block"
-          router-link="/game-setup"
-          class="ion-button-success"
-          size="large"
-        >
-          <ion-icon slot="start" name="gamepad"></ion-icon>
-          {{ $t('home.startGame') }}
-          <span slot="end" class="text-xl">🎮</span>
-        </ion-button>
+      <div>
+        <div class="flex flex-col gap-4 m-auto" style="width: 280px">
+          <!-- Start Game Button -->
+          <ion-button
+            expand="block"
+            router-link="/game-setup"
+            class="ion-button-success"
+            size="large"
+          >
+            <ion-icon slot="start" name="gamepad"></ion-icon>
+            {{ $t('home.startGame') }}
+            <span slot="end" class="text-xl">🎮</span>
+          </ion-button>
 
-        <!-- Manage Roles Button -->
-        <ion-button 
-          expand="block"
-          router-link="/roles"
-          fill="outline"
-          size="large"
-        >
-          <ion-icon slot="start" name="card"></ion-icon>
-          {{ $t('home.manageRoles') }}
-          <span slot="end" class="text-xl">🃏</span>
-        </ion-button>
+          <!-- Manage Roles Button -->
+          <ion-button
+            expand="block"
+            router-link="/roles"
+            fill="outline"
+            size="large"
+          >
+            <ion-icon slot="start" name="card"></ion-icon>
+            {{ $t('home.manageRoles') }}
+            <span slot="end" class="text-xl">🃏</span>
+          </ion-button>
 
-        <!-- Manage Players Button -->
-        <ion-button 
-          expand="block"
-          router-link="/players"
-          fill="outline"
-          size="large"
-        >
-          <ion-icon slot="start" name="people"></ion-icon>
-          {{ $t('home.managePlayers') }}
-          <span slot="end" class="text-xl">👥</span>
-        </ion-button>
+          <!-- Manage Players Button -->
+          <ion-button
+            expand="block"
+            router-link="/players"
+            fill="outline"
+            size="large"
+          >
+            <ion-icon slot="start" name="people"></ion-icon>
+            {{ $t('home.managePlayers') }}
+            <span slot="end" class="text-xl">👥</span>
+          </ion-button>
 
-        <!-- Settings Button (Disabled) -->
-        <ion-button 
-          expand="block"
-          disabled
-          fill="outline"
-          size="large"
-        >
-          <ion-icon slot="start" name="settings"></ion-icon>
-          {{ $t('home.viewStats') }}
-          <span slot="end" class="text-xl">⚙️</span>
-        </ion-button>
+          <!-- Settings Button (Disabled) -->
+          <ion-button
+            expand="block"
+            disabled
+            fill="outline"
+            size="large"
+          >
+            <ion-icon slot="start" name="settings"></ion-icon>
+            {{ $t('home.viewStats') }}
+            <span slot="end" class="text-xl">⚙️</span>
+          </ion-button>
+        </div>
       </div>
     </ion-content>
   </ion-page>
